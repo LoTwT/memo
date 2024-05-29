2adf9a453fe8534426c423bdd2ce55aefd068c7c
Lo <l709937065@gmail.com>
2024-05-29 11:05:56 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,4 @@ import { defineConfig } from "@ayingott/eslint-config"
 
 export default defineConfig({
   typescript: true,
-  formatters: true,
-  markdown: true,
-  rules: {
-    "vue/singleline-html-element-content-newline": "off",
-    "style/operator-linebreak": "off",
-  },
 })
